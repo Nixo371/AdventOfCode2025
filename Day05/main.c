e8442846bb302e09d0a249162cac96a055c95905
Nicolas Ucieda <nicolas.ucieda@gmail.com>
2025-12-05 13:21:30 +0100

    Day 5 Part 2

--- Day05/main.c
+++ Day05/main.c
@@ -1,8 +1,11 @@
+#include <locale.h>
 #include <stddef.h>
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+
+#define DEBUG 0
 
 typedef struct s_range {
 	int64_t start;
@@ -51,7 +54,7 @@ char* get_next_line(FILE* file) {
 }
 
 void print_range(range r) {
-	printf("%ld - %ld", r.start, r.end);
+	printf("%'ld - %'ld", r.start, r.end);
 }
 
 void print_ranges(range* ranges, int range_count) {
@@ -90,17 +93,138 @@ range parse_range(char* line) {
 	return (r);
 }
 
+// returns which range it's in
+// -1 if in none
 int is_fresh(range* ranges, int range_count, int64_t id) {
 	for (int i = 0; i < range_count; i++) {
 		if (id >= ranges[i].start && id <= ranges[i].end) {
-			return (1);
+			return (i);
 		}
 	}
 
-	return (0);
+	return (-1);
+}
+
+int64_t get_total_fresh_ingredients(range* ranges, int range_count) {
+	int range;
+	for (int i = 0; i < range_count; i++) {
+		range = is_fresh(ranges, i, ranges[i].start);
+		// while the start of a range is inside a previous range
+		while (range != -1) {
+			if (DEBUG) {
+				printf("Range %d\n(", i);
+				print_range(ranges[i]);
+				printf(") overlaps with range %d\n(", range);
+				print_range(ranges[range]);
+				printf(")\nAdjusting\n(");
+				print_range(ranges[i]);
+				printf(") to\n(");
+			}
+
+			ranges[i].start = ranges[range].end + 1;
+			if (DEBUG) {
+				print_range(ranges[i]);
+				printf(")\n");
+			}
+
+			// Total overlap, set range to "null"
+			if (ranges[i].start > ranges[i].end) {
+				ranges[i].start = -1;
+				ranges[i].end = -1;
+
+				if (DEBUG) {
+					printf("Total Overlap!\n");
+				}
+				break;
+			}
+
+			range = is_fresh(ranges, i, ranges[i].start);
+		}
+
+		if (ranges[i].start == -1 && ranges[i].end == -1) {
+			if (DEBUG) {
+				printf("------\n");
+			}
+			continue;
+		}
+
+		range = is_fresh(ranges, i, ranges[i].end);
+		// while the end of a range is inside a previous range
+		while (range != -1) {
+			if (DEBUG) {
+				printf("Range %d\n(", i);
+				print_range(ranges[i]);
+				printf(") overlaps with range %d\n(", range);
+				print_range(ranges[range]);
+				printf(")\nAdjusting\n(");
+				print_range(ranges[i]);
+				printf(") to\n(");
+			}
+
+			ranges[i].end = ranges[range].start - 1;
+			if (DEBUG) {
+				print_range(ranges[i]);
+				printf(")\n");
+			}
+
+			// Total overlap, set range to "null"
+			if (ranges[i].end < ranges[i].start) {
+				ranges[i].start = -1;
+				ranges[i].end = -1;
+
+				break;
+			}
+
+			range = is_fresh(ranges, i, ranges[i].end);
+		}
+
+
+		// Detect having a smaller range completely enclosed inside, and killing it
+		for (int j = 0; j < i; j++) {
+			if (ranges[i].start < ranges[j].start && ranges[i].end > ranges[j].end) {
+				if (DEBUG) {
+					printf("Range %d\n(", i);
+					print_range(ranges[i]);
+					printf(") completely encloses range %d\n(", j);
+					print_range(ranges[j]);
+					printf(")\nKilling range %d\n", j);
+				}
+
+				ranges[j].start = -1;
+				ranges[j].end = -1;
+			}
+		}
+
+		if (DEBUG) {
+			printf("------\n");
+		}
+	}
+
+	if (DEBUG) {
+		printf("New Ranges:\n");
+		print_ranges(ranges, range_count);
+	}
+
+	int64_t total_fresh_ingredients = 0;
+	for (int i = 0; i < range_count; i++) {
+		if (ranges[i].start == -1 && ranges[i].end == -1) {
+			continue;
+		}
+
+		int64_t ingredients = (ranges[i].end - ranges[i].start) + 1;
+		if (DEBUG) {
+			printf("Range (");
+			print_range(ranges[i]);
+			printf("): %ld ingredients\n", ingredients);
+		}
+		total_fresh_ingredients += ingredients;
+	}
+
+	return (total_fresh_ingredients);
 }
 
 int main(int argc, char *argv[]) {
+	setlocale(LC_ALL, "");
 	char* file_name = "input";
 	if (argc > 1) {
 		file_name = argv[1];
@@ -139,13 +263,15 @@ int main(int argc, char *argv[]) {
 	for (char* line = get_next_line(input); line != NULL; line = get_next_line(input)) {
 		int64_t id = atol(line);
 		
-		if (is_fresh(ranges, range_count, id)) {
+		if (is_fresh(ranges, range_count, id) != -1) {
 			fresh_ingredients += 1;
 		}
 
 		free(line);
 	}
 
-	printf("Fresh Ingredients: %d\n", fresh_ingredients);
+	int64_t total_fresh_ingredients = get_total_fresh_ingredients(ranges, range_count);
+	printf("Fresh Ingredients (Part 1): %d\n", fresh_ingredients);
+	printf("Total Fresh Ingredients (Part 2): %ld\n", total_fresh_ingredients);
 }
 
