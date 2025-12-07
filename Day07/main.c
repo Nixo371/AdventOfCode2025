c776d06f5371432ee127590de8575b691ffd13ad
Nicolas Ucieda <nicolas.ucieda@gmail.com>
2025-12-07 13:49:16 +0100

    Day 7 Part 2

--- Day07/main.c
+++ Day07/main.c
@@ -1,4 +1,5 @@
 #include <stddef.h>
+#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -20,8 +21,16 @@ int main(int argc, char *argv[]) {
 	size_t rows;
 	size_t columns;
 	char** grid = parse_grid(input, &rows, &columns);
+	int64_t** paths = (int64_t **) malloc(rows * sizeof(int64_t *));
+	for (size_t i = 0; i < rows; i++) {
+		paths[i] = (int64_t *) malloc(columns * sizeof(int64_t));
+		for (size_t j = 0; j < columns; j++) {
+			paths[i][j] = 0;
+		}
+	}
 
 	size_t splits = 0;
+	size_t total_paths = 0;
 	for (size_t row = 0; row < rows; row++) {
 		for (size_t column = 0; column < columns; column++) {
 			char c = grid_get(grid, row, column, rows, columns);
@@ -30,14 +39,23 @@ int main(int argc, char *argv[]) {
 				printf("Error getting grid[%lu][%lu]\n", row, column);
 			}
 
-			if ((c == 'S' || c == '|') && grid_get(grid, row + 1, column, rows, columns) == '.') {
-				// if out of bounds we don't do anything so we can leave this unhandled
-				grid_set(grid, row + 1, column, rows, columns, '|');
+			if (c == 'S') {
+				paths[row][column] = 1;
+			}
+			if ((c == 'S' || c == '|') && grid_get(grid, row + 1, column, rows, columns) != '^') {
+				if (grid_set(grid, row + 1, column, rows, columns, '|') == 0) {
+					paths[row + 1][column] += paths[row][column];
+				}
 			}
 			else if ((c == 'S' || c == '|') && grid_get(grid, row + 1, column, rows, columns) == '^') {
 				// if out of bounds we don't do anything so we can leave this unhandled
-				grid_set(grid, row + 1, column - 1, rows, columns, '|');
-				grid_set(grid, row + 1, column + 1, rows, columns, '|');
+				if (grid_set(grid, row + 1, column - 1, rows, columns, '|') == 0) {
+					paths[row + 1][column - 1] += paths[row][column];
+				}
+
+				if (grid_set(grid, row + 1, column + 1, rows, columns, '|') == 0) {
+					paths[row + 1][column + 1] += paths[row][column];
+				}
 
 				splits += 1;
 			}
@@ -51,6 +69,11 @@ int main(int argc, char *argv[]) {
 		}
 	}
 
+	for (size_t column = 0; column < columns; column++) {
+		total_paths += paths[rows - 1][column];
+	}
+
 	printf("Times Split (Part 1): %lu\n", splits);
+	printf("Different Paths (Part 2): %lu\n", total_paths);
 }
 
